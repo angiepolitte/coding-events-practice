a12fc0ba8ae25c7adc037c254d7ae277de9d70ec
Angie <angiepolitte@gmail.com>
2025-01-05 14:30:34 -0600

    Initial Commit

---
+++ src/main/java/org/launchcode/coding_events_practice/CodingEventsPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.coding_events_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingEventsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingEventsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/coding_events_practice/CodingEventsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.coding_events_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
